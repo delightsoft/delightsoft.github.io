2e5b4bfa9ca4654bf37750d2451340b3a9607a6a
Alexey Zorkaltsev <alexey@zorkaltsev.com>
2015-05-10 11:21:06 +0300

    update

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./src/app/app.coffee":[function(require,module,exports){
+require('./ui/yandex-metrika');
+
+require('./ui/google-analytics');
+
 require('./ng-app');
 
 
 
-},{"./ng-app":"C:\\SVN\\_DelightSoftSite\\src\\app\\ng-app.coffee"}],"C:\\SVN\\_DelightSoftSite\\src\\app\\ng-app.coffee":[function(require,module,exports){
+},{"./ng-app":"C:\\SVN\\_DelightSoftSite\\src\\app\\ng-app.coffee","./ui/google-analytics":"C:\\SVN\\_DelightSoftSite\\src\\app\\ui\\google-analytics.coffee","./ui/yandex-metrika":"C:\\SVN\\_DelightSoftSite\\src\\app\\ui\\yandex-metrika.coffee"}],"C:\\SVN\\_DelightSoftSite\\src\\app\\ng-app.coffee":[function(require,module,exports){
 var ngModule;
 
 module.exports = (ngModule = angular.module('app', ['ngRoute', require('./ui/ui'), require('./tableOfContent')])).name;
 
 ngModule.run([
   '$rootScope', '$location', (function($rootScope, $location) {
+    var firstTime;
     $rootScope.title = '';
     $rootScope.urlStartsWith = (function(s) {
       return $location.path().startsWith(s);
@@ -17,6 +22,15 @@ ngModule.run([
     $rootScope.toggleSidebar = (function() {
       $('body').toggleClass('hide-video');
     });
+    firstTime = true;
+    $rootScope.$on('$routeChangeSuccess', (function(ev) {
+      if (firstTime) {
+        firstTime = false;
+      } else {
+        yaCounter30123629.hit($location.path(), $rootScope.pageTitle);
+        ga('send', 'pageview', $location.path());
+      }
+    }));
   })
 ]);
 
@@ -67,27 +81,6 @@ ngModule.config([
     }).when('/dscommon/server', {
       templateUrl: '/tmpl/dscommon/server/index.html',
       controller: setPageTitle('DelightSoft.ru / DSCommon Сервер')
-    }).when('/dscommon/server/code-generation', {
-      templateUrl: '/tmpl/dscommon/server/code-generation.html',
-      controller: setPageTitle('DelightSoft.ru / Механизм кодогенерации')
-    }).when('/dscommon/server/database', {
-      templateUrl: '/tmpl/dscommon/server/database.html',
-      controller: setPageTitle('DelightSoft.ru / База данных')
-    }).when('/dscommon/server/cost', {
-      templateUrl: '/tmpl/dscommon/server/cost.html',
-      controller: setPageTitle('DelightSoft.ru / Стоимость')
-    }).when('/dscommon/server/quality', {
-      templateUrl: '/tmpl/dscommon/server/quality.html',
-      controller: setPageTitle('DelightSoft.ru / Качество')
-    }).when('/dscommon/server/rights-managment', {
-      templateUrl: '/tmpl/dscommon/server/rights-managment.html',
-      controller: setPageTitle('DelightSoft.ru / Система прав')
-    }).when('/dscommon/server/workflow', {
-      templateUrl: '/tmpl/dscommon/server/workflow.html',
-      controller: setPageTitle('DelightSoft.ru / Реализованы все основные механизмы документооборота')
-    }).when('/dscommon/jsapp', {
-      templateUrl: '/tmpl/dscommon/jsapp/index.html',
-      controller: setPageTitle('DelightSoft.ru / DSCommon JSApp')
     }).when('/projects', {
       templateUrl: '/tmpl/projects/index.html',
       controller: setPageTitle('DelightSoft.ru / Проекты')
@@ -115,6 +108,27 @@ ngModule.config([
     }).when('/test', {
       templateUrl: '/tmpl/test/index.html',
       controller: setPageTitle('DelightSoft.ru / Редактируемая статья')
+    }).when('/test/jsapp', {
+      templateUrl: '/tmpl/test/jsapp/index.html',
+      controller: setPageTitle('DelightSoft.ru / DSCommon JSApp')
+    }).when('/test/code-generation', {
+      templateUrl: '/tmpl/test/code-generation.html',
+      controller: setPageTitle('DelightSoft.ru / Механизм кодогенерации')
+    }).when('/test/database', {
+      templateUrl: '/tmpl/test/database.html',
+      controller: setPageTitle('DelightSoft.ru / База данных')
+    }).when('/test/cost', {
+      templateUrl: '/tmpl/test/cost.html',
+      controller: setPageTitle('DelightSoft.ru / Стоимость')
+    }).when('/test/quality', {
+      templateUrl: '/tmpl/test/quality.html',
+      controller: setPageTitle('DelightSoft.ru / Качество')
+    }).when('/test/rights-managment', {
+      templateUrl: '/tmpl/test/rights-managment.html',
+      controller: setPageTitle('DelightSoft.ru / Система прав')
+    }).when('/test/workflow', {
+      templateUrl: '/tmpl/test/workflow.html',
+      controller: setPageTitle('DelightSoft.ru / Реализованы все основные механизмы документооборота')
     });
   })
 ]);
@@ -124,6 +138,26 @@ ngModule.run([
     $rootScope.pageTitle = 'DelightSoft.ru';
   })
 ]);
+
+
+
+},{}],"C:\\SVN\\_DelightSoftSite\\src\\app\\ui\\google-analytics.coffee":[function(require,module,exports){
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments);
+  };
+  i[r].l = 1 * new Date;
+  a = s.createElement(o);
+  m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-48478932-2', 'auto');
+
+ga('send', 'pageview');
 
 
 
@@ -255,6 +289,40 @@ ngModule.directive('toggleMenu', [
     };
   })
 ]);
+
+
+
+},{}],"C:\\SVN\\_DelightSoftSite\\src\\app\\ui\\yandex-metrika.coffee":[function(require,module,exports){
+(function(d, w, c) {
+  var f, n, s;
+  (w[c] = w[c] || []).push(function() {
+    var e;
+    try {
+      w.yaCounter30123629 = new Ya.Metrika({
+        id: 30123629,
+        webvisor: true,
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true
+      });
+    } catch (_error) {
+      e = _error;
+    }
+  });
+  n = d.getElementsByTagName('script')[0];
+  s = d.createElement('script');
+  f = function() {
+    n.parentNode.insertBefore(s, n);
+  };
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = (d.location.protocol === 'https:' ? 'https:' : 'http:') + '//mc.yandex.ru/metrika/watch.js';
+  if (w.opera === '[object Opera]') {
+    d.addEventListener('DOMContentLoaded', f, false);
+  } else {
+    f();
+  }
+})(document, window, 'yandex_metrika_callbacks');
 
 
 
